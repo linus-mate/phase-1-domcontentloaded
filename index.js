1d2ccbfc9707064122105744652bdc6085c776b4
linus mutugi mate <mlynux@gmail.com>
2022-08-22 13:02:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("After DOM Loaded")
+    document.getElementById("text")
+    text.innerHTML = "This is really cool!" ;
+    //text.innerHTML = text.innerHTML.replace('hello');
+  });
